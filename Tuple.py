9f6061d1c6c33bbdc3f5be69ed971c3be84b95cd
mahiratuba8 <mahiratuba8@gmail.com>
2024-11-29 19:05:46 +0100

    changes

---
+++ Tuple.py
@@ -0,0 +1,14 @@
+a=()
+print(a)
+
+#tuple having intergers
+b=(1,2,2,4,5,7)
+print(b)
+
+c=(1,"mahira",True,-8)
+print(type(c))
+print(c)
+
+d=("a","b","c","d",[1,2,3,4,5,6,7],(11,12,13,14,15))
+print(d)
+print(d[1:])
